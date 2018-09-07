9bff6e77de14a9c0aef86c815a2c79f4b797b4f5
Eliot Swank <eliotswank@gmail.com>
2018-09-07 16:17:45 -0600

    update hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
-  render html: "hello, world"
+  protect_from_forgery, with: :exception
+
+  def hello
+    render html: "Hello World"
+  end
+  
 end
