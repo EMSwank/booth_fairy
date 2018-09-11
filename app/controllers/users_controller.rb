09863af0a7a3e89545687173980c912d65f08d21
Eliot Swank <eliotswank@gmail.com>
2018-09-11 11:39:03 -0600

    styles weather card

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def index
     @user = current_user
     weather = WeatherService.new('Denver')
-    @current_weather = [weather.current_temp, 
+    @current_weather = [weather.current_temp, weather.condition]
   end
 end
--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,28 +1,22 @@
 require 'barometer'
 
 class WeatherService
-  attr_reader :five_day, :current_temp, :condition
+  attr_reader :current_temp, :condition
 
   def initialize(query)
     @query = query
-    @five_day = five_day
   end
 
   def get_weather
     weather = Barometer.new(@query)
     weather.measure
-    binding.pry
   end
 
   def current_temp
     get_weather.responses.first.current.temperature.f
   end
 
-  def five_day
-    get_weather.forecast
-  end
-
   def condition
-    get_weather.responses.first.condition
+    get_weather.responses.first.current.condition
   end
 end
