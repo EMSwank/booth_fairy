9578fd318354667ff461577ae78076d9ca6ef37e
Eliot Swank <eliotswank@gmail.com>
2018-09-14 12:27:57 -0600

    adds current weather to user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,8 @@ class UsersController < ApplicationController
   def index
     @user = current_user
     @conn = Faraday.new(url: "https://api.darksky.net")
-    response = @conn.get("/forecast/#{ENV[DARK_SKY_API_KEY]}/39.7391428,-104.984696")
-    @weather = JSON.parse(response.body, symbolize_names: true)[:results]
-    
-    binding.pry
-    
+    response = @conn.get("/forecast/#{ENV["DARK_SKY_API_KEY"]}/39.7391428,-104.984696")
+    @weather = JSON.parse(response.body, symbolize_names: true)
+
   end
 end
--- spec/features/user_sees_user_index_page_spec.rb
+++ spec/features/user_sees_user_index_page_spec.rb
@@ -1,25 +1,26 @@
 require 'rails_helper'
 
 describe "User index" do
-  before(:each) do
-    user = create(:user)
-    visit new_user_session_path
-
-    fill_in :user_email,	with: user.email
-    fill_in :user_password,	with: user.password
-
-    within ".actions" do
-      click_on "Log in"
-    end
-  end
-
+  
   context "shows data:" do
+    before(:each) do
+      @user = create(:user)
+      visit new_user_session_path
+  
+      fill_in :user_email,	with: @user.email
+      fill_in :user_password,	with: @user.password
+  
+      within ".actions" do
+        click_on "Log in"
+      end
+    end
+
     it "current_temperature" do
 
       visit user_root_path
-
-      expect(page).to have_content(user.email)
-      expect(page).to have_content("Today's Weather")
+save_and_open_page
+      expect(page).to have_content(@user.email)
+      expect(page).to have_content("Current Weather")
       expect(page).to have_css(".forecast")
     end
   end
