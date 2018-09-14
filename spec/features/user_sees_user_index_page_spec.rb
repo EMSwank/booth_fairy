7aa909eeb8525bbedb8ee335e5a45d47d7b69eac
Eliot Swank <eliotswank@gmail.com>
2018-09-14 11:01:49 -0600

    refactors test

--- spec/features/user_sees_user_index_page_spec.rb
+++ spec/features/user_sees_user_index_page_spec.rb
@@ -1,9 +1,20 @@
 require 'rails_helper'
 
 describe "User index" do
-  context "shows data" do
+  before(:each) do
+    user = create(:user)
+    visit new_user_session_path
+
+    fill_in :user_email,	with: user.email
+    fill_in :user_password,	with: user.password
+
+    within ".actions" do
+      click_on "Log in"
+    end
+  end
+
+  context "shows data:" do
     it "current_temperature" do
-      user = create(:user)
 
       visit user_root_path
 
