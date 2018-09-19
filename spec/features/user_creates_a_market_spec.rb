f6ad40d17b942a5704a2b479396325b257d7d525
Eliot Swank <eliotswank@gmail.com>
2018-09-19 08:40:36 -0600

    adds test for new market story

---
+++ spec/features/user_creates_a_market_spec.rb
@@ -0,0 +1,40 @@
+
+describe "User creates a market" do
+  context "by visiting the market new page" do
+    before(:each) do
+      @user = create(:user)
+      visit new_user_session_path
+
+      fill_in :user_email,	with: @user.email
+      fill_in :user_password,	with: @user.password
+
+      VCR.use_cassette("features/user_creates_a_market") do
+        within ".actions" do
+          click_on "Log in"
+        end
+    end
+
+    it "and filling out a form" do
+      visit new_market_path
+
+      fill_in :name,	with: "Name-1"
+      fill_in :street_address, with: "address 1"
+
+      click_on 'Create New Market'
+
+      expect(current_path).to eq(markets_path)
+      expect(page).to have_content("Name-1")
+    end
+  end
+end
+
+
+
+
+=begin
+User visits the Market New page
+They see a form with the market name (drop-down?)
+as well as a field for address and a submit button.
+After they fill out the form and click "Create New Market"
+They're redirected to the market index page.
+=end
