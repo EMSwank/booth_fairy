8927949dc9dd9696871a3632c8a87f9bebf13869
Eliot Swank <eliotswank@gmail.com>
2018-09-19 00:16:33 -0600

    adds new market button

---
+++ app/controllers/markets_controller.rb
@@ -0,0 +1,4 @@
+class MarketsController < ApplicationController
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'markets/new'
   get 'users/index'
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'welcome#index'
